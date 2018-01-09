e484983ab457318bfd8639e85e4b81cb72972a04
Simon Thiboutôt <simonthiboutot@hotmail.com>
2018-01-09 07:53:53 -0500

    Finish lastfm feature.
    Get artist.
    
    Get artist albums.
    
    Get album.

--- app/index.js
+++ app/index.js
@@ -1,21 +1,64 @@
 const LastfmService = require('./services/lastfm-service')
+const _ = require('lodash')
+const cors = require('cors')
 const express = require('express')
-const app = express()
-
-const lastfm = new LastfmService(process.env.LAST_FM_KEY)
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const app = express()
+const lastfm = new LastfmService(process.env.LAST_FM_KEY)
 const PORT = process.env.PORT || 3000
+
+app.use(cors())
 
 app.get('/', (req, res) => res.send('Trevor API'))
 
 app.get('/search', (req, res) => {
-  lastfm.search(req.query.query)
+  const query = req.query.query
+  if (_.isEmpty(query)) {
+    return req(400).json({ error: 'Required params `query`' })
+  }
+  lastfm.search(query)
     .then((results) => res.status(200).json(results))
     .catch((error) => res(400).json({ error }))
+})
+
+app.get('/albums/:mbid', (req, res) => {
+  const mbid = req.params.mbid
+  if (_.isEmpty(mbid)) {
+    return req(400).json({ error: 'Required params `mbid`' })
+  }
+  lastfm.getAlbum(mbid)
+    .then((album) => res.status(200).json(album))
+    .catch((error) => {
+      return req(400).json({ error: error.message })
+    })
+})
+
+app.get('/artists/:mbid', (req, res) => {
+  const mbid = req.params.mbid
+  if (_.isEmpty(mbid)) {
+    return req(400).json({ error: 'Required params `mbid`' })
+  }
+  lastfm.getArtist(mbid)
+    .then((artist) => res.status(200).json(artist))
+    .catch((error) => {
+      return req(400).json({ error: error.message })
+    })
+})
+
+app.get('/artists/:mbid/albums', (req, res) => {
+  const mbid = req.params.mbid
+  if (_.isEmpty(mbid)) {
+    return req(400).json({ error: 'Required params `mbid`' })
+  }
+  lastfm.getArtistAlbums(mbid)
+    .then((albums) => res.status(200).json(albums))
+    .catch((error) => {
+      return req(400).json({ error: error.message })
+    })
 })
 
 app.listen(PORT, () => console.log(`Trevor API listening on port ${PORT}`))
--- app/services/lastfm-service.js
+++ app/services/lastfm-service.js
@@ -1,22 +1,7 @@
 const _ = require('lodash')
-const request = require('request')
+const Lastfm = require('../../lib/lastfm.js')
 
-function Lastfm (apiKey) {
-  this.apiKey = apiKey
-}
-
-Lastfm.prototype.request = function (method, params = {}) {
-  const options = {
-    url: 'http://ws.audioscrobbler.com/2.0/',
-    qs: Object.assign(params, { api_key: this.apiKey, method, format: 'json' })
-  }
-  return new Promise((resolve, reject) => {
-    request(options, (error, response, body) => {
-      if (error) return reject(error)
-      resolve(JSON.parse(body))
-    })
-  })
-}
+const UUID_REGEX = /[a-f0-9]{8}-?[a-f0-9]{4}-?[1-5][a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}/
 
 function LastfmService (apiKey) {
   this.client = new Lastfm(apiKey)
@@ -25,6 +10,43 @@ function LastfmService (apiKey) {
 LastfmService.prototype.retrieveImage = function (object, size = 'large') {
   const image = object.image.find((image) => image['size'] === size)
   if (image) return image['#text']
+}
+
+LastfmService.prototype.getAlbum = function (mbid) {
+  return this.client.request('album.getinfo', { mbid }).then((data) => {
+    const album = _.get(data, 'album', {})
+    const result = _.pick(album, ['name', 'artist', 'mbid', 'url'])
+    result.image = this.retrieveImage(album)
+    result.type = 'album'
+    result.summary = _.get(album, 'wiki.summary', '')
+    return result
+  })
+}
+
+LastfmService.prototype.getArtist = function (mbidOrArtist) {
+  const key = UUID_REGEX.exec(mbidOrArtist) ? 'mbid' : 'artist'
+  return this.client.request('artist.getinfo', { [key]: mbidOrArtist }).then((data) => {
+    const artist = _.get(data, 'artist', {})
+    const result = _.pick(artist, ['name', 'artist', 'mbid', 'url'])
+    result.image = this.retrieveImage(artist)
+    result.type = 'artist'
+    result.summary = _.get(artist, 'bio.summary', '')
+    return result
+  })
+}
+
+LastfmService.prototype.getArtistAlbums = function (mbidOrArtist) {
+  const key = UUID_REGEX.exec(mbidOrArtist) ? 'mbid' : 'artist'
+  return this.client.request('artist.gettopalbums', { [key]: mbidOrArtist, limit: 999 }).then((data) => {
+    const albums = _.get(data, 'topalbums.album', {})
+    return albums.reduce((results, album) => {
+      const result = _.pick(album, ['name', 'mbid', 'url'])
+      result['artist'] = _.get(album, 'artist.name')
+      result['image'] = this.retrieveImage(album)
+      if (!_.isEmpty(result.mbid)) results.push(result)
+      return results
+    }, [])
+  })
 }
 
 LastfmService.prototype.searchAlbum = function (query) {
